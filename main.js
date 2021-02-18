354fa2d0154e8892ed8b776c3fece612c20cbddd
Olu Olageshin <44141085+lagesh@users.noreply.github.com>
2021-02-18 00:52:49 +0000

    refactor javascript

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-function myFunction() {
-  let burger = document.querySelector(".mobile");
-  let dropDown = document.querySelector('.nav-dropdown');
-  burger.addEventListener('click', function () {
-    dropDown.style.display = 'block';
-  })
+let burgerMenu = document.querySelector('.mobile');
+
+let myFunction = () => {
+  burgerMenu.classList.toggle('nav-dropdown')
 }
-myFunction();
+
+burgerMenu.onclick = myFunction;
